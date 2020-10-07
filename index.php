de8bb49ed544d186c1aba39f271c8f079002793a
Irwin-S <irwin.r.singh@gmail.com>
2020-10-07 11:16:23 -0600

    Added header/footer to index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<p>Hello, World!</p>
+<?php get_header(); // WP function to grab header.php?>
+
+<p>This is <strong>index.php</strong></p>
+
+<?php get_footer(); // WP function to grab footer.php?>
+
