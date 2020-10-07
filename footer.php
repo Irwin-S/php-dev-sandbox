0566ff129cf548360fe92e86a9adaa96c90b2738
Irwin-S <irwin.r.singh@gmail.com>
2020-10-07 11:12:49 -0600

    Added footer information

--- footer.php
+++ footer.php
@@ -1,2 +1,18 @@
+    <footer>
+    <h4><?php bloginfo( 'title' ); ?></h4>
+        <p>
+            &copy; 
+            <?php date( 'Y' ); ?>
+            <a href="<?php echo site_url(); ?>">
+                <?php bloginfo( 'title' ); ?>
+            </a>
+            All Rights Reserved.
+        </p>
+    </footer>
+    <?php
+        // Similiar to wp_head(), but for footer scripts
+        // and output.
+        wp_footer();
+    ?>
 </body>
 </html>
